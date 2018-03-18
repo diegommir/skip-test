f40e275a86f6eb0989ebf5221cc5e5dfa6e7c5da
Diego Miranda <diegommir@gmail.com>
2018-03-18 14:39:16 -0300

    Adding @RestController annotation to Order and Item controllers.

--- src/main/java/com/example/diegommir/skiptest/controller/ItemController.java
+++ src/main/java/com/example/diegommir/skiptest/controller/ItemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.diegommir.skiptest.business.OrderBO;
 import com.example.diegommir.skiptest.entity.Item;
@@ -25,6 +26,7 @@ import com.example.diegommir.skiptest.repository.ItemRepository;
  * 
  * @author Diego Miranda
  */
+@RestController
 public class ItemController {
 	@Autowired
 	private OrderBO orderBO;
--- src/main/java/com/example/diegommir/skiptest/controller/OrderController.java
+++ src/main/java/com/example/diegommir/skiptest/controller/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.diegommir.skiptest.business.BusinessBO;
 import com.example.diegommir.skiptest.business.OrderBO;
@@ -27,6 +28,7 @@ import com.example.diegommir.skiptest.repository.OrderRepository;
  * 
  * @author Diego Miranda
  */
+@RestController
 public class OrderController {
 	@Autowired
 	private BusinessBO businessBO;
